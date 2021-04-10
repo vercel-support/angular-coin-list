ec9e932a107171890a65980238b1f6fa46f64a8d
Jagan Sahu <jagannsahu@gmail.com>
2021-04-10 01:40:40 -0400

    fix functionality and add style changes

--- src/app/components/coin-lister/coin-lister.component.ts
+++ src/app/components/coin-lister/coin-lister.component.ts
@@ -8,27 +8,65 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./coin-lister.component.css']
 })
 export class CoinListerComponent implements OnInit {
-  btcCoin: any; //make an interface to use specifically 
-  constructor(private api: ApiService) { 
-    
-  }
-  ngOnInit(): void {
-    this.getCoinData()
+  searchterm: string = '';
+  allCoins: string[] = [];
+  allCoinsData: any[] = [];
+  holdAllCoinsData: any[] = [];
+  
+  constructor(private api: ApiService) {   
   }
 
-  getCoinData() {
-    this.api.getCoinData()
-      .subscribe((data: any) => {
-        console.log("DATA ACCESSED!")
-        console.log(data)
-        this.btcCoin = data;
-        console.log(this.btcCoin)
-      },
-      (error) => {
-        console.log("ERROR!")
+  ngOnInit(): void {
+    this.getAllCoins()
+    this.holdAllCoinsData = this.allCoinsData;
+  }
+
+  filterCoin(searchterm: string) {
+    let filteredCoins: any[] = [];
+    this.allCoinsData.forEach(e => {
+      if (e.name.includes(searchterm)) {
+        filteredCoins.push(e)
+      }
+    });
+    this.allCoinsData = filteredCoins;
+  }
+
+  onEnter(searchterm: string) {
+    this.allCoinsData = this.holdAllCoinsData;
+    this.searchterm = searchterm;
+    this.filterCoin(this.searchterm.toUpperCase())
+  }
+
+  getAllCoins() {
+    this.api.getAllCoins()
+      .subscribe((coinList: any) => {
+        console.log('SUCCESS!!!')
+        this.allCoins = coinList;
+        for (let i = 0; i < 100; i++) {
+          //20 possible requests per refresh so we don't run into errors (max req per min for the Api is around 90)
+          if (this.allCoins[i].substring(3, 6) == "usd") {
+            this.getCoinData(this.allCoins[i])
+          }
+        }
+      }, (error) => {
+        console.log('ERROR!!!')
         console.log(error)
       })
   }
+
+  getCoinData(coin: string) {
+    this.api.getCoinData(coin)
+      .subscribe((coinData) => {
+        console.log("SUCCESS 2!!")
+        let name = {"name" : coin.toUpperCase()}
+        let updatedCoinData = Object.assign(coinData, name)
+        this.allCoinsData.push(updatedCoinData)
+      }, (error) => {
+        console.log("ERROR 2!!!")
+        console.log(error)
+      })
+  }
+
 
   
 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,10 +5,15 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
-  readonly url = 'https://api.cryptonator.com/api/ticker/btc-usd'; //use bitfinex api and use proxy to get thru cors errors
+  readonly allCoinsUrl = 'https://api.bitfinex.com/v1/symbols';
   constructor(private _http: HttpClient) { }
 
-  getCoinData() {
-    return this._http.get(this.url)
+  getAllCoins() {
+    return this._http.get(this.allCoinsUrl)
   }
+
+  getCoinData(coin: string) {
+    return this._http.get(`https://api.bitfinex.com/v1/pubticker/${coin}`);
+  }
+  
 }
