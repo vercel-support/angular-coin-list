2c9a5676d090203b93da3c76846be4f21041dbf0
Jagan Sahu <jagannsahu@gmail.com>
2021-04-11 00:23:25 -0400

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoinListerComponent } from '../app/components/coin-lister/coin-lister.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CoinListerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
